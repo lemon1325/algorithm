e41d6d051bd3dd9829aeb97b5ca4dc02a57ced5b
김나연 <155406368+lemon1325@users.noreply.github.com>
2024-12-28 23:02:36 +0900

    Title: 문자에 따른 명령 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241228/문자에 따른 명령 2/text-based-commands2.cpp
+++ 241228/문자에 따른 명령 2/text-based-commands2.cpp
@@ -14,10 +14,10 @@ int main() {
 
     for(int i=0;i<str.size();i++){
         if(str[i]=='L'){
-            dir=(dir+1)%4;
+            dir=(dir-1)%4;
         }
         else if(str[i]=='R'){
-            dir=(dir-1)%4;
+            dir=(dir+1)%4;
         }
         else if(str[i]=='F'){
             x+=offset[dir][0];
