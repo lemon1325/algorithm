d544cfc15c8999dccd6463f22d99fbadc3b7cf3b
김나연 <155406368+lemon1325@users.noreply.github.com>
2025-02-20 14:47:07 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -4,23 +4,25 @@
 using namespace std;
 
 int main() {
-	int r = 31, m = 1234567891;
+	unsigned long r = 31, m = 1234567891;
 
 	int n;
 	cin >> n;
 	string str;
 	cin >> str;
-	vector<int>alphaToIntNums;
+	vector<unsigned long>alphaToIntNums;
 
 	//숫자로 변환
 	for (int i = 0; i < n; i++) {
 		alphaToIntNums.push_back((int)str[i]-96);
 	}
 	
-	int sum = 0;
-	for (int i = 0, k=1; i < n; i++,k*=r) {
-		sum += alphaToIntNums[i] * k;
-		sum = sum % m;
+	unsigned long long sum = 0;
+	unsigned long long num = 1;
+	for (int i = 0; i < n; i++) {
+		sum = (sum+(alphaToIntNums[i] * num)%m) % m;
+		num = (num * r) % m;
+		
 	}
 
 	cout << sum;
