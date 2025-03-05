8eb93d0299da018ff2c921c8e762b7018958d4e9
김나연 <155406368+lemon1325@users.noreply.github.com>
2025-03-05 09:14:36 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; i++) {
+		cout << "Material Management " << i << endl;
+		int n, a, b; //a는 크기 제한, b는 무게 제한
+		cin >> n >> a >> b;
+		for (int j = 0; j < n; j++) {
+			int u,v;
+			cin >> u >> v;
+
+		}
+		cout << "Classification ---- End!" << endl;
+	}
+
+	return 0;
+}
